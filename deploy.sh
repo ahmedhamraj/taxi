ade4633a4d74fa99f6d95ca43752fc834bebb91e
ahmed <ahmedhamraj12@gmail.com>
2025-11-30 18:48:55 +0530

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Replace IMAGE_TAG with BUILD_NUMBER
+# Replace IMAGE_TAG
 sed -i "s|IMAGE_TAG|${BUILD_NUMBER}|g" deployment.yaml
 
-# Apply manifests
+# Apply deployment & service
 kubectl apply -f deployment.yaml -n dev
 kubectl apply -f service.yaml -n dev
 
-# Status
+# Rollout status
+kubectl rollout status deployment/taxiapp -n dev
+
+# Show pods and service
 kubectl get pods -n dev
 kubectl get svc -n dev
-kubectl get deployments -n dev
